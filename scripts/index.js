a239bf88267a50c8c9074c7aabd581bf4cebfb4f
Maxim Pribytkov <pribytkovmaksim919@gmail.com>
2025-01-03 17:39:16 +0500

    release v1.1

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,17 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardsTemplate = document.querySelector('#card-template').content;
+const cardsList = document.querySelector('.places__list');
+  for (let i = 0; i < initialCards.length; i++) {
+    const cloneCards = cardsTemplate.querySelector('.places__item').cloneNode(true);
+    const deleteButton = cloneCards.querySelector('.card__delete-button');
+    const listItem = deleteButton.closest('.places__item');
+    cloneCards.querySelector('.card__image').src = initialCards[i].link;
+    cloneCards.querySelector('.card__title').textContent = initialCards[i].name;
+    cardsList.append(cloneCards);
+    deleteButton.addEventListener('click', function () {
+      listItem.remove();
+    });
+    };
